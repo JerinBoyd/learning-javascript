f2825df1f6565c00a393fdaffbe54c2a3eefbc38
JerinBoyd <maxers@mac.com>
2018-01-30 18:25:03 -0500

    indroduced and explored if, else, else-if

--- comparisons-and-conditions.js
+++ comparisons-and-conditions.js
@@ -21,4 +21,36 @@ console.log('aNumber < anotherNumber: ', aNumber < anotherNumber);
 console.log('aNumber <== anotherNumber: ', aNumber <= anotherNumber);
 console.log('aNumber !== anotherNumber: ', aNumber !== anotherNumber);
 
+// conditional statements
+// if -> using it to run code based on the answer to a question
+// if-else => use it to run code based on the answer to a question 
+       // but these are a fallback plan (the else)
+// if-elseif-else -> i have multiple questions that determine what code to run
 
+// NOTE; There is another way called a 'switch' but it runs differently
+
+// 'if' 
+if(3 === 3){
+    console.log('Orange crush is delicious!');
+}
+
+if('a' === 'b'){
+    console.log('this will never print');
+}
+
+//if-else
+if('grape' === 'banana'){
+    console.log('this is never going to run');
+} else{
+    console.log('this is a backup plan');
+}
+
+//if-elseif-else
+const someNumber = 100;
+if(someNumber !== 100){
+  console.log('it is 100');
+} else if(someNumber === 42){
+    console.log('the meaining of life');
+} else {
+    console.log('none of the others worked');
+}
