11b1c0bf9770b49274ebf878edae2b0c4f3ff960
JerinBoyd <maxers@mac.com>
2018-02-13 18:39:42 -0500

    added functions to move x and y then ran console.log-this to show what this means

--- objects-and-classes/objects.js
+++ objects-and-classes/objects.js
@@ -8,11 +8,18 @@ const circle = {
         circle.color = newColor;
     },
     grow: function(){
-        circle.radius += 1;
+        this.radius += 1;
     },
     shrink: function(){
-        circle.radius -= 1;
+        this.radius -= 1;
     },
+    // move: function(moveX){
+    //     circle.x = += 1;
+    // }
+    move: function(dx, dy){
+       this.x += dx;
+       this.y += dy;
+    }
 
     
 };
@@ -24,3 +31,6 @@ circle.shrink();
 console.log(circle);
 circle.grow();
 console.log(circle);
+circle.move(10, 10);
+console.log(circle);
+console.log(this);
