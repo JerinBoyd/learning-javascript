ef243ffdf7a21f90a4535740af246329aad34797
JerinBoyd <maxers@mac.com>
2018-02-13 19:45:13 -0500

    created a truck class that demonstrates the extends the super keywords that allows the truck to be a vehicl ebut have unique truck properties and mthods on top of the car properties and methods

--- objects-and-classes/classes.js
+++ objects-and-classes/classes.js
@@ -4,14 +4,36 @@ class Vehicle {
         this.stank = stank;
 
     }
+    drive(){
+        console.log('vrrrroooommmmmmmm!!!!!!');
+    }
 }
 
 const v = new Vehicle();
 const v2 = new Vehicle('yello', 'old car smell');
 console.log(v);
+v.drive();
 
 const vLiteral = {
     color: 'red',
     stank: 'new car'
 };
 console.log(vLiteral);
+
+class truck extends Vehicle {
+    constructor(color='red', stank='new stank', hasLiftKit=true){
+        super(color, stank);
+        this.hasLiftKit = hasLiftKit;
+    }
+    goMudding(){
+        console.log('ssuuuueeeyyyy!!!!');
+    }
+    drive(){
+        console.log('bbbbbaaarrrahhhhhhh!!!');
+    }
+}
+
+const t = new truck();
+t.drive();
+t.goMudding();
+console.log(t);
